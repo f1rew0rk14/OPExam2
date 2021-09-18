bbd95b67f370257cd4a8bc75facc1b89075b0573
f1rew0rk14 <81588841+f1rew0rk14@users.noreply.github.com>
2021-09-18 18:11:33 +0300

    Add files via upload

---
+++ src.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+template<class T>
+class Auto_ptr
+{
+	T* m_ptr;
+public:
+	Auto_ptr(T* ptr = nullptr)
+		:m_ptr(ptr)
+	{
+	}
+
+	~Auto_ptr()
+	{
+		delete m_ptr;
+	}
+
+	Auto_ptr(Auto_ptr& a) 
+	{
+		m_ptr = a.m_ptr;
+		a.m_ptr = nullptr;
+	}
+
+	Auto_ptr& operator=(Auto_ptr& a)
+	{
+		if (&a == this)
+			return *this;
+
+		delete m_ptr;
+		m_ptr = a.m_ptr;
+		a.m_ptr = nullptr; 
+		return *this;
+	}
+
+	T& operator*() const { return *m_ptr; }
+
+};
